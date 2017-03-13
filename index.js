b8be82a873dbf7e427fe3bcbfd955eb5cce3223c
Sönke Kluth <mail@soenkekluth.com>
2017-03-13 16:28:24 +0100

    process exit

--- index.js
+++ index.js
@@ -5,35 +5,48 @@ const fs = require('fs');
 const commandExists = require('command-exists');
 const argv = require('minimist')(process.argv.slice(2));
 
+// console.log(argv);
+
 var command = argv._[0];
+// const rest = argv._.slice(1);
+
+// console.log('rest', rest);
 
 var options = Object.keys(argv).map(key => {
+  // console.log(key);
   if (key !== '_') {
     return '-' + key + ' ' + argv[key];
   }
 });
+
+
+const target = process.argv.slice(3).join(' ');
 
 options.splice(0, 1);
 if (options.length) {
   command += ' ' + options.join(' ');
 }
 
+
+// console.log('command', command+ ' '+rest.join(' '));
+// console.log('target', target);
+
 const shell = command => new Promise((resolve, reject) => {
   var out = '';
   exec(command, { stdio: 'inherit' }, (error, stdout, stderr) => {
     if (error) {
-      resolve(error);
+      resolve(stdout);
       return;
     }
 
     if (stderr) {
-      out = stderr;
+      // out = stderr;
     }
     out += stdout;
 
     resolve(out);
   });
-});
+})
 
 commandExists('yarn', function(err, exists) {
   let cmd = 'npm run';
@@ -41,5 +54,11 @@ commandExists('yarn', function(err, exists) {
     cmd = 'yarn run';
   }
   cmd += ' ' + command;
-  shell(cmd);
+  shell(cmd)
+    .then((out)=> {
+      console.log(out); process.end(1);
+    })
+    .catch(e => {
+      console.log(e); process.end(1);
+    })
 });
